1cce7d5ce95bfd310179933e6ba7e18b7e96f969
vcky4 <okon.victor.u@gmail.com>
2025-05-14 14:03:37 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/victor/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  // optimizeDeps: {
+  //   exclude: ['lucide-react'],
+  // },
 });
