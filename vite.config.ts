11c79129277bde3705d8f5e5c0c539bd96c759da
vcky4 <okon.victor.u@gmail.com>
2025-05-14 12:42:14 +0100

    change name to victor

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/victor-portfolio/',
+  base: '/victor/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
